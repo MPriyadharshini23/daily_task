1e5e731690e33afca4cb4ec198bbb95a70510603
Priyadharshini M <mpriyadharshinicse23@gmail.com>
2026-03-21 23:40:43 +0530

    Add files via upload

---
+++ firstNonRepeat.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class firstNonRepeat{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<n;i++){
+            if(map.get(arr[i])==1){
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+}
